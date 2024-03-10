6cb213fc1951efb8c0c7b02797289ccba20df6ef
IMTIAJ HOSSAIN <emon1609bd@gmail.com>
2024-03-10 16:17:07 +0600

    Create 88. Merge Sorted Array.py

---
+++ Top Interview 150/88. Merge Sorted Array.py
@@ -0,0 +1,28 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # Solution - 1
+        # i = m
+        # for num in nums2:
+        #     nums1[i] = num
+        #     i += 1
+
+        # nums1.sort()
+        
+        # Solution - 2 (more optimal)
+        i, j, k = m - 1, n - 1, m + n - 1
+
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j, k = j - 1, k - 1
