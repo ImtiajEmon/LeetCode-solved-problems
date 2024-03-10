998472af29586e55c078ffaa9861d756a97bd621
IMTIAJ HOSSAIN <emon1609bd@gmail.com>
2024-03-10 17:53:34 +0600

    Update 167. Two Sum II - Input Array Is Sorted.py

--- Randomly Solved/167. Two Sum II - Input Array Is Sorted.py
+++ Randomly Solved/167. Two Sum II - Input Array Is Sorted.py
@@ -1,23 +1,34 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        def binary_search(arr, key):
-            l, r = 0, len(arr)-1
+        # def binary_search(arr, key):
+        #     l, r = 0, len(arr)-1
 
-            while l <= r:
-                mid = (l + r) // 2
+        #     while l <= r:
+        #         mid = (l + r) // 2
 
-                if key > arr[mid]:
-                    l = mid + 1
-                elif key < arr[mid]:
-                    r = mid - 1
-                else:
-                    return mid
+        #         if key > arr[mid]:
+        #             l = mid + 1
+        #         elif key < arr[mid]:
+        #             r = mid - 1
+        #         else:
+        #             return mid
 
-            return -1
+        #     return -1
 
-        for i in range(len(numbers)-1):
-            candidate = target - numbers[i]
-            ret = binary_search(numbers[i+1:], candidate)
-            if ret != -1:
-                ans = [i+1, ret+2+i]
-                return ans
+        # for i in range(len(numbers)-1):
+        #     candidate = target - numbers[i]
+        #     ret = binary_search(numbers[i+1:], candidate)
+        #     if ret != -1:
+        #         ans = [i+1, ret+2+i]
+        #         return ans
+
+        #======================================================
+        # More Optimal Solution
+        i, j = 0, len(numbers) - 1
+        while i <= j:
+            if target > numbers[i] + numbers[j]:
+                i += 1
+            elif target < numbers[i] + numbers[j]:
+                j -= 1
+            else:
+                return [i+1, j+1]
